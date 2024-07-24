1a0567a89222d7a579b3d5a9cec88d1cc1fdd4f9
Claudinei Nascimento <claudinei.nascimento@e-deploy.com.br>
2024-07-24 16:59:18 -0300

    Code improvements

--- CleanArchitecture.Persistence/Context/DataContext.cs
+++ CleanArchitecture.Persistence/Context/DataContext.cs
@@ -1,4 +1,6 @@
 ﻿using CleanArchitecture.Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Data;
 
 namespace CleanArchitecture.Persistence.Context;
 
@@ -7,8 +9,11 @@ public class DataContext : DbContext, IApplicationDbContext
     public DbSet<User> User { get; set; }
 
     private readonly IMediator _mediator;
+    private IDbContextTransaction _currentTransaction;
     private readonly AuditableEntitySaveChangesInterceptor _auditableEntitySaveChangesInterceptor;
 
+    public IDbContextTransaction GetCurrentTransaction() => _currentTransaction;
+    public bool HasActiveTransaction => _currentTransaction != null;
 
     public DataContext(
         DbContextOptions<DataContext> options,
@@ -19,6 +24,7 @@ public class DataContext : DbContext, IApplicationDbContext
         _mediator = mediator;
         _auditableEntitySaveChangesInterceptor = auditableEntitySaveChangesInterceptor;
     }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
@@ -35,5 +41,55 @@ public class DataContext : DbContext, IApplicationDbContext
     {
         await _mediator.DispatchDomainEvents(this);
         return await base.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<IDbContextTransaction> BeginTransactionAsync()
+    {
+        if (_currentTransaction != null) return null;
+
+        _currentTransaction = await Database.BeginTransactionAsync(IsolationLevel.ReadCommitted);
+
+        return _currentTransaction;
+    }
+
+    public async Task CommitTransactionAsync(IDbContextTransaction transaction)
+    {
+        ArgumentNullException.ThrowIfNull(transaction);
+        if (transaction != _currentTransaction) throw new InvalidOperationException($"Transaction {transaction.TransactionId} is not current");
+
+        try
+        {
+            await SaveChangesAsync();
+            await transaction.CommitAsync();
+        }
+        catch
+        {
+            RollbackTransaction();
+            throw;
+        }
+        finally
+        {
+            if (_currentTransaction != null)
+            {
+                _currentTransaction.Dispose();
+                _currentTransaction = null;
+            }
+        }
+    }
+
+    public void RollbackTransaction()
+    {
+        try
+        {
+            _currentTransaction?.Rollback();
+        }
+        finally
+        {
+            if (_currentTransaction != null)
+            {
+                _currentTransaction.Dispose();
+                _currentTransaction = null;
+            }
+        }
     }
 }
